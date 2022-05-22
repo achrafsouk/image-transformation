06c0bd3b5be71d30f517c5d647d451a30808357c
achrafsouk <48942933+achrafsouk@users.noreply.github.com>
2022-05-22 20:18:51 +0200

    Create image-transformation.ts

---
+++ bin/image-transformation.ts
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import { ImageTransformationStack } from '../lib/image-transformation-stack';
+
+
+const app = new cdk.App();
+new ImageTransformationStack(app, 'myImageTransformationStack', {
+
+});
+
