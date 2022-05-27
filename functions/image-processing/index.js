090c9ba5cbcabc18221e3598b49f0f2d928b46e1
achrafsouk <48942933+achrafsouk@users.noreply.github.com>
2022-05-27 21:59:43 +0200

    Update index.js

--- functions/image-processing/index.js
+++ functions/image-processing/index.js
@@ -3,27 +3,32 @@ const https = require('https');
 const Sharp = require('sharp');
 
 const keepAliveAgent = new https.Agent({keepAlive: true});
-const S3 = new AWS.S3({signatureVersion: 'v4',httpOptions: {agent: keepAliveAgent}}); //TODO, is it still needed?
+const S3 = new AWS.S3({signatureVersion: 'v4',httpOptions: {agent: keepAliveAgent}}); 
 const S3_BUCKET = process.env.bucketName; 
 const TRANSFORMED_IMAGE_PREFIX = process.env.transformedImagePrefix;
+const TRANSFORMED_IMAGE_CACHE_TTL = process.env.transformedImageCacheTTL;
 const SECRET_KEY = process.env.secretKey;
-const TRANSFORMED_IMAGE_TTL = process.env.transformedImageTTL;
+const STORE_TRANSFORMED_IMAGES = process.env.storeTransformedImages;
+const LOG_TIMING = process.env.logTiming;
 
 exports.handler = async (event) => {
     // First validate if the request is coming from CloudFront
-    if (!event.headers["x-origin-secret-header"] || !(event.headers["x-origin-secret-header"] === SECRET_KEY)) return sendError(403, 'Request unathorized', event);
+    if (!event.headers['x-origin-secret-header'] || !(event.headers['x-origin-secret-header'] === SECRET_KEY)) return sendError(403, 'Request unathorized', event);
     // Validate if this is a GET request
     if (!event.requestContext || !event.requestContext.http || !(event.requestContext.http.method === 'GET')) return sendError(400, 'Only GET method is supported', event);
 
     // The request triggering this function used the */images/* cache behavio.
-    // An example of expected path is /format=auto,width=100/images/cats/cat1.jpg
-    var imagePathArray= event.requestContext.http.path.split("/");
-    imagePathArray.shift(); // Get rid of first slash
+    // An example of expected path is /hashValue/format=auto,width=100/images/rio/1.jpg
+    var imagePathArray= event.requestContext.http.path.split('/');
+    imagePathArray.shift(); // Get rid of first slash 
+    imagePathArray.shift(); // remove the hash 
     // get the prefix which corresponds to requested image operatioons
     var operationsPrefix = imagePathArray.shift(); 
-    console.log('operationsPrefix', operationsPrefix);
     // with the prefix removed, we get the original image path on the S3 bucket
     var originalImagePath = imagePathArray.join('/');
+    // timing variable
+    var timingLog = "perf ";
+    var startTime = performance.now();
     // Downloading original image
     let originalImage;
     let contentType;
@@ -43,13 +48,15 @@ exports.handler = async (event) => {
         var operationKV = operation.split("=");
         operationsJSON[operationKV[0]] = operationKV[1];
     });
-
+    timingLog = timingLog + (performance.now()-startTime) + ' ';
+    startTime = performance.now();
     try {
         // check if resizing is requested
         var resizingOptions = {};
         if (operationsJSON['width']) resizingOptions.width = parseInt(operationsJSON['width']);
         if (operationsJSON['height']) resizingOptions.height = parseInt(operationsJSON['height']);
-        if (resizingOptions) transformedImage = await sharpObject.resize(resizingOptions);
+        if (resizingOptions) transformedImage = sharpObject.resize(resizingOptions);
+        // check if formatting is requested
         if (operationsJSON['format']) {
             var isLossy = false;
             switch (operationsJSON['format'])
@@ -63,42 +70,45 @@ exports.handler = async (event) => {
                default : contentType = 'image/jpeg'; isLossy = true;
             }
             if (operationsJSON['quality'] && isLossy) {
-                transformedImage = await transformedImage.toFormat(operationsJSON['format'], {
+                transformedImage = transformedImage.toFormat(operationsJSON['format'], {
                     quality: parseInt(operationsJSON['quality']),
                 });
-            } else transformedImage = await transformedImage.toFormat(operationsJSON['format']);
+            } else transformedImage = transformedImage.toFormat(operationsJSON['format']);
         }
         transformedImage = await transformedImage.toBuffer();
     } catch (error) {
         console.log(error);
         return sendError(500, 'error transforming image', event);
     }
-
-    // upload transformed image back to S3
-    try { 
-        await S3.putObject({
-            Body: transformedImage, 
-            Bucket: S3_BUCKET, 
-            Key:  TRANSFORMED_IMAGE_PREFIX + event.requestContext.http.path, 
-            StorageClass: "ONEZONE_IA",
-            ContentType: contentType,
-            
-            Metadata: {
-                'cache-control': 'max-age='+TRANSFORMED_IMAGE_TTL,
-            },
-        }, function(err, data) {});
-    } catch (error) {
-        console.log('APPLICATION ERROR', 'Could not upload transformated image to S3');
-        console.log(JSON.stringify(event));
+    timingLog = timingLog + (performance.now()-startTime) + ' ';
+    startTime = performance.now();
+    // upload transformed image back to S3 if required in the architecture
+    if (STORE_TRANSFORMED_IMAGES === 'true') {
+        try { 
+            await S3.putObject({
+                Body: transformedImage, 
+                Bucket: S3_BUCKET, 
+                Key:  TRANSFORMED_IMAGE_PREFIX + event.requestContext.http.path, 
+                ContentType: contentType,
+                Metadata: {
+                    'cache-control': TRANSFORMED_IMAGE_CACHE_TTL,
+                },
+            }, function(err, data) {});
+        } catch (error) {
+            console.log('APPLICATION ERROR', 'Could not upload transformated image to S3');
+            console.log(error);
+        }
     }
-
+    timingLog = timingLog + (performance.now()-startTime) + ' ';
+    if (LOG_TIMING === 'true') console.log(timingLog);
+    // return transformed image
     return {
         statusCode: 200,
         body: transformedImage.toString('base64'),
         isBase64Encoded: true,
         headers: {
             'Content-Type': contentType, 
-            'Cache-Control': 'max-age='+TRANSFORMED_IMAGE_TTL 
+            'Cache-Control': TRANSFORMED_IMAGE_CACHE_TTL 
         }
     };
 };
@@ -111,4 +121,3 @@ function sendError(code, message, event){
         body: message,
     };
 }
-
